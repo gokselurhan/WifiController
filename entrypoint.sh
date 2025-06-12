a56f611e99b959160e061ed7ea421152b02de035
Göksel Urhan <123533112+gokselurhan@users.noreply.github.com>
2025-06-12 20:13:15 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Kernel seviyesinde NAT routing ve forwarding açık mı
 sysctl -w net.ipv4.ip_forward=1
 
+# iptables NAT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-service hostapd start
+# İlk başta hostapd config yoksa başlatılmasın
+if [ ! -f /etc/hostapd/hostapd.conf ]; then
+  echo "hostapd config /etc/hostapd/hostapd.conf not found, not starting hostapd.."
+else
+  hostapd -B /etc/hostapd/hostapd.conf
+fi
 
 python app.py
