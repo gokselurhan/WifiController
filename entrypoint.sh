72de5ddf342ccf3d4bc95f003ee2efc89a08b749
Göksel Urhan <123533112+gokselurhan@users.noreply.github.com>
2025-06-21 01:02:55 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,6 @@ set -e
 DHCP_SERVERS=${DHCP_RELAY_SERVERS:-192.168.1.1}
 UPLINK_IFACE=${UPLINK_INTERFACE:-eth0}
 
-# VLAN desteği için kernel modülünü yükle
-if ! grep -q 8021q /proc/modules; then
-    echo "802.1q modülü yükleniyor..."
-    if [ -f /lib/modules/$(uname -r)/kernel/net/8021q/8021q.ko ]; then
-        insmod /lib/modules/$(uname -r)/kernel/net/8021q/8021q.ko
-        echo "8021q" >> /etc/modules
-    else
-        echo "Uyarı: 8021q modülü bulunamadı!"
-    fi
-fi
-
 # DHCP Relay konfigürasyonu
 echo "DHCP Relay konfigürasyonu yapılıyor..."
 cat > /etc/default/isc-dhcp-relay <<EOF
